48a8f28da7779d29af590617bd47b7ee1a47df30
Виктория Аксентий <peta-holding@yandex.ru>
2021-04-14 21:22:14 +0300

    test yandex

--- homework.py
+++ homework.py
@@ -8,10 +8,36 @@ import requests
 import telegram
 from dotenv import load_dotenv
 
-from logger_conf import LOGGING_CONFIG
-
 load_dotenv()
 
+LOGGING_CONFIG = {
+        'version':1,
+        'handlers':{
+            'fileHandler':{
+                'class': 'logging.handlers.RotatingFileHandler',
+                'formatter':'myFormatter',
+                'filename':'my_logger.log',
+                'maxBytes': 50000000,
+                'backupCount': 5,
+                'encoding':'utf-8'
+            }
+        },
+        'loggers':{
+            'info':{
+                'handlers':['fileHandler'],
+                'level':'INFO',
+            },
+            'debug':{
+                'handlers':['fileHandler'],
+                'level':'DEBUG',
+            }
+        },
+        'formatters':{
+            'myFormatter':{
+                'format':'%(asctime)s, %(levelname)s, %(message)s'
+            }
+        }
+    }
 logging.config.dictConfig(LOGGING_CONFIG)
 logger = logging.getLogger('info')
 logger = logging.getLogger('debug')
